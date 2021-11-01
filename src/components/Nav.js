99c7d1b05af74d88c321d4b7681e7be970fae48b
jay4195 <jay4195@qq.com>
2021-11-01 22:45:34 +0800

    实现导航栏基本模式，精简icon设计

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import { HomeIcon, HomeIconSelected, MessageIcon, MoreIcon, NotificationIcon, ProfileIcon, SharpIcon, SharpIconSelected, TwitterIcon } from "./Icons";
 
@@ -42,88 +43,95 @@ const NavWrapper = styled.div`
   }
 `;
 
-
 const Nav = () => {
   const [navId, setNavId] = useState(0);
+  const history = useHistory();
 
-  const clickLeftButton = () => {
-      setNavId(1);
-      console.log(navId);
-  };
+  var start = history.location.pathname.split("/").filter(item => item != '')[0];
+
+  var tabId = 0;
+  if (start === undefined || start === "home") {
+    tabId = 0;
+  } else if (start === "explore") {
+    tabId = 1;
+  } else if (start === "nofications") {
+    tabId = 2;
+  } else if (start === "messages") {
+    tabId = 3;
+  } else {
+    tabId = 4;
+  }
+
+  if (tabId !== navId) {
+    setNavId(tabId);
+  }
 
   return (
-    <NavWrapper>
-      <nav>
-        <a href = "/">
-            <div class = "nav-section" >
-                <TwitterIcon/>
-            </div>
-        </a>
-        <a href = "/">
-            {navId === 0 && (
-            <div class = "nav-section" onClick={clickLeftButton}>
-                <HomeIconSelected/>
-                <div class = "nav-name">
-                    <span>Home</span>
-                </div>
-            </div>)}
-            {navId !== 0 &&(
-            <div class = "nav-section">
-                <HomeIcon/>
-                <div class = "nav-name">
-                    <span>Home</span>
-                </div>
-            </div>)}
-        </a>
-        <a href = "/explore">
-        {navId === 1 &&(
-            <div class = "nav-section">
-                <SharpIconSelected/>
-                <div class = "nav-name">
-                    <span>Explore</span>
-                </div>
-            </div>)}
-            {navId !== 1 &&(
-            <div class = "nav-section">
-                <SharpIcon/>
-                <div class = "nav-name">
-                    <span>Explore</span>
-                </div>
-            </div>)}
-        </a>
-        <a href = "/notifications">
+  <NavWrapper>
+    <nav>
+      <a href = "/">
+        <div class = "nav-section" >
+            <TwitterIcon/>
+        </div>
+      </a>
+      <a href = "/home">
         <div class = "nav-section">
+          <HomeIcon select={navId === 0}/>
+          <div class = "nav-name">
+              {navId === 0 && (<b>Home</b>)}
+              {navId !== 0 && (<span >Home</span>)}
+          </div>
+        </div>
+      </a>
+      <a href = "/explore">
+        {navId === 1 && (
+        <div class = "nav-section" >
+            <SharpIconSelected/>
+            <div class = "nav-name">
+                <b>Explore</b>
+            </div>
+        </div>)}
+        {navId !== 1 &&(
+        <div class = "nav-section">
+            <SharpIcon/>
+            <div class = "nav-name">
+                <span>Explore</span>
+            </div>
+        </div>)}
+      </a>
+      <a href = "/notifications">
+        <div class = "nav-section">
+
             <NotificationIcon/>
             <div class = "nav-name">
                 <span>Notifications</span>
             </div>
         </div>
-        </a>
-        <a href = "/messages">
-            <div class = "nav-section">
-                <MessageIcon/>
-                <div class = "nav-name">
-                    <span>Messages</span>
-                </div>
-            </div>
-        </a>
-        {/* TODO 修改成用户的id */}
-        <a href = "/profile">
-            <div class = "nav-section">
-                <ProfileIcon/>
-                <div class = "nav-name">
-                    <span>Profile</span>
-                </div>
-            </div>
-        </a>
+      </a>
+      <a href = "/messages">
         <div class = "nav-section">
-            <MoreIcon/>
+            <MessageIcon/>
             <div class = "nav-name">
-                <span>More</span>
-            </div>  
+                <span>Messages</span>
+            </div>
         </div>
-      </nav>
-    </NavWrapper>
+      </a>
+      <a href = "/profile">
+        <div class = "nav-section">
+            <ProfileIcon/>
+            <div class = "nav-name">
+                <span>Profile</span>
+            </div>
+        </div>
+      </a>
+      <div class = "nav-section">
+        <MoreIcon/>
+        <div class = "nav-name">
+            <span>More</span>
+        </div>  
+      </div>
+    </nav>
+  </NavWrapper>
   );
 };
 
